2958dc409e25a6ddf45e2c531f40c0490ce2b537
danielG-blip <jgiraldo1@cesde.net>
2026-04-22 16:26:54 -0500

    Creacion proyecto

---
+++ src/main/java/com/entrega_2/luking/LukingApplication.java
@@ -0,0 +1,13 @@
+package com.entrega_2.luking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LukingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entrega_2/luking/LukingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entrega_2.luking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
