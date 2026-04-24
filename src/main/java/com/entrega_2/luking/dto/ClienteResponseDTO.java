c56d96b03bb02ff5db39abffe29ce6e5d4d2be54
danielG-blip <jgiraldo1@cesde.net>
2026-04-24 15:38:43 -0500

    Creacion clienteResponse

--- src/main/java/com/entrega_2/luking/dto/ClienteResponseDTO.java
+++ src/main/java/com/entrega_2/luking/dto/ClienteResponseDTO.java
@@ -1,5 +1,10 @@
 package com.entrega_2.luking.dto;
 
+import lombok.Data;
+
+@Data
 public class ClienteResponseDTO {
-    
+    private Long id;
+    private String nombre;
+    private String email;
 }
