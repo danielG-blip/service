a42f5c55281bc9d4719efc163d637722aac71570
danielG-blip <jgiraldo1@cesde.net>
2026-04-24 15:55:39 -0500

    Creacion expedition

---
+++ src/main/java/com/entrega_2/luking/expedition/ClienteExpedition.java
@@ -0,0 +1,7 @@
+package com.entrega_2.luking.expedition;
+
+public class ClienteExpedition extends RuntimeException{
+    public ClienteException(String mensaje) {
+        super(mensaje);
+    }
+}
